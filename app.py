09419ba6420ee9bdee7866ff76c5191288f0e8be
gustavosalomon <77517342+gustavosalomon@users.noreply.github.com>
2025-06-20 17:11:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 # Conexion a MongoDB Atlas
-MONGO_URI = "mongodb+srv://admin:admin123@cluster0.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URI = "mongodb://admin:admin123@cluster0-shard-00-00.2owahcw.mongodb.net:27017,cluster0-shard-00-01.2owahcw.mongodb.net:27017,cluster0-shard-00-02.2owahcw.mongodb.net:27017/smart_parking?ssl=true&replicaSet=atlas-13p8m5-shard-0&authSource=admin&retryWrites=true&w=majority"
 client = MongoClient(MONGO_URI)
 db = client['smart_parking']
 
